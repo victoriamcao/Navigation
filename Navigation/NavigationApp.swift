b561d2ed9571e6dfd4498d169edd5054c8de7b1a
Vicky Cao <miavickcao@gmail.com>
2025-07-24 09:28:47 -0700

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Scholar on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
